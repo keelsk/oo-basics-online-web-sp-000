667c725f9216ad8284b0ce5c69fab4f673c7f43b
Kirsten Keels <keelsk@gmail.com>
2020-02-28 20:06:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,34 +1,12 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
   
   def title
     @title
-  end
-  
-  def author=(author_name)
-    @author = author_name
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(pages)
-    @page_count = pages
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
   end
   
   def turn_page
